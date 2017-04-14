48b797ce1537235a8883f29c1c9678d164195982
Mineralo4ka <andrew.pavlenko@mail.ru>
2017-04-14 17:02:33 +0700

    Start programm, ver 0.1

---
+++ Makefile
@@ -0,0 +1,2 @@
+all: main.c
+	gcc -Wall -o cmdcalc main.c
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(char stroke[30])
+{
+	printf("===================\n");
+	printf("Welcome to cmdCalc!\n");
+	printf("===================\n");
+
+	return 0;
+}
